48cf9951de3f27c0255ed16dcd124d760afc68a4
fabianobbats <fabianobatista@outlook.com>
2016-12-11 13:52:28 -0400

    Create 1001 - Extremamente Básico.c
    Resolução URI 1001 - Extremamente Básico

---
+++ 1001 - Extremamente Básico.c
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------
+	Nome: 1001 - Extremamente Básico
+	Autor: Fabiano Batista
+	Data: 11/12/2016
+---------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main(){
+	int a, b, x;
+	scanf("%d %d", &a, &b);
+	x = a + b;
+	printf("%d", x);
+	return 0;
+}
